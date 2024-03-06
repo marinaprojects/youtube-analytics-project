1f6951cfa3a6632409314c515edbf2d35d4019a6
marinaprojects <155964514+marinaprojects@users.noreply.github.com>
2024-03-06 14:28:15 -0600

    changed channel.py

--- src/channel.py
+++ src/channel.py
@@ -1,16 +1,12 @@
-from pprint import pprint
+from googleapiclient import channel
 from googleapiclient.discovery import build
 from dotenv import load_dotenv
-from settings import FILE_NAME
-from helper.youtube_api_manual import channel
 
 import os
 import json
+from pprint import pprint
 
-load_dotenv(FILE_NAME)
 load_dotenv()
-moscowpython_list = []
-
 
 class Channel:
     """Класс для ютуб-канала"""
@@ -19,88 +15,41 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.__channel_id = channel_id
-        channel = self.get_service().channels().list(id=self.__channel_id, part='snippet,statistics').execute()
-        self.channel_name = channel['items'][0]['snippet']['title']
-        self.channel_desc = channel['items'][0]['snippet']['description']
-        self.channel_link = f'https://www.youtube.com/channel/{channel["items"][0]["id"]}'
-        self.channel_sub_count = int(channel['items'][0]['statistics']['channel_sub_count'])
-        self.channel_video_count = channel['items'][0]['statistics']['videoCount']
-        self.channel_view_count = channel['items'][0]['statistics']['viewCount']
+        #self.sub_count = int(self.channel_id['items'][0]['statistics']['subscriberCount'])
+        #self.video_count = self.channel_data['items'][0]['statistics']['videoCount']
         self.youtube = build('youtube', 'v3', developerKey=self.api_key)
-
-
-    def __str__(self) -> str:
-        """возвращает строковое представление канала"""
-        return f"{self.channel_name}, ({self.channel_link})"
-
-
-    def __add__(self, other) -> int:
-        """возвращает сумму подписчиков двух каналов"""
-        return self.channel_sub_count + other.channel_sub_count
-
-
-    def __sub__(self, other) -> int:
-        """возвращает разницу подписчиков двух каналов"""
-        return self.channel_sub_count - other.channel_sub_count
-
-
-    def __sub__(self, other) -> int:
-        """возвращает разницу подписчиков двух каналов"""
-        return self.channel_sub_count - other.channel_sub_count
-
-
-    def __gt__(self, other) -> bool:
-        """возвращает True, если количество подписчиков текущего
-         канала больше, чем у другого"""
-        return self.channel_sub_count > other.channel_sub_count
-
-
-    def __ge__(self, other) -> bool:
-        """возвращает True, если количество подписчиков текущего
-         канала больше или равно, чем у другого"""
-        return self.channel_sub_count >= other.channel_sub_count
-
-
-    def __lt__(self, other)-> bool:
-        """возвращает True, если количество подписчиков текущего
-         канала меньше чем у другого"""
-        return self.channel_sub_count < other.channel_sub_count
-
-
-    def __le__(self, other) -> bool:
-        """возвращает True, если количество подписчиков текущего
-         канала меньше или равно, чем у другого"""
-        return self.channel_sub_count <= other.channel_sub_count
-
-    def __eq__(self, other) -> bool:
-        """возвращает True, если количество подписчиков текущего
-         канала равно, чем у другого"""
-        return self.channel_sub_count == other.channel_sub_count
-
-    def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
-        self.channel = self.get_service().channels().list(id=self.__channel_id, part='snippet,statistics').execute()
-        pprint(channel)
-
-    def to_json(self, file_name: str):
-        data = json.dumps(self, channel)
-        with open(file_name, 'w', encoding='utf-8') as file:
-            file.write(data)
-
-
-    @property
-    def channel_id(self):
-        return self.__channel_id
-
-    @property
-    def title(self):
-        return f'{self.channel["items"][0]["snippet"]["title"]}'
-
-    @property
-    def view_count(self):
-        return f'{moscowpython_list["items"][0]["statistics"]["viewCount"]}'
+        self.channel_data = self.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        self.url = f'https://www.youtube.com/channel/{self.channel_data["items"][0]["id"]}'
 
     @classmethod
     def get_service(cls):
         youtube = build('youtube', 'v3', developerKey=cls.api_key)
         return youtube
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
+
+    @channel_id.setter
+    def channel_id(self, value):
+        self.__channel_id = value
+
+    @property
+    def channel(self):
+        return self.channel_data
+
+    @property
+    def title(self):
+        return self.channel_data['items'][0]['snippet']['title']
+
+    @property
+    def video_count(self):
+        return self.channel_data['items'][0]['statistics']['viewCount']
+
+    def print_info(self) -> None:
+        """Выводит в консоль информацию о канале."""
+        pprint(self.channel_data)
+
+    def to_json(self, file_name: str):
+        with open(file_name, 'w', encoding='utf-8') as file:
+            json.dump(self.channel_data, file)
