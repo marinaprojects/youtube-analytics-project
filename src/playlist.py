686a692b18748d9788444640c25df9f39db1a3f0
marinaprojects <155964514+marinaprojects@users.noreply.github.com>
2024-03-19 17:20:58 -0500

    changed playlist.py

--- src/playlist.py
+++ src/playlist.py
@@ -1,5 +1,6 @@
 import os
 import datetime
+import isodate  # Для разбора ISO 8601 длительности
 
 from dotenv import load_dotenv
 from googleapiclient.discovery import build
@@ -21,38 +22,46 @@ class PlayList:
         self.playlist_data = None
         self.video_response = None
         self.title = None
-        #self.title: str = self.playlist_data()['items'][0]['snippet']['title']
         # получить все id видеороликов из плейлиста
         self.video_ids = []
+        # Вызов метода для заполнения информации о плейлисте
+        self.fetch_playlist_info()
 
-    def playlist_response(self) -> None:
-        """если информации в словаре нет, возвращает информацию о плейлисте"""
+    def fetch_playlist_info(self):
+        """Получить информацию о плейлисте"""
         if self.playlist_data is None:
-            self.playlist_data = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                       playlist_dataid=self.playlist_data).execute()
-            return self.playlist_data
+            self.playlist_data = self.youtube.playlists().list(part='snippet', id=self.playlist_id).execute()
+            if 'items' in self.playlist_data and len(self.playlist_data['items']) > 0:
+                self.title = self.playlist_data['items'][0]['snippet']['title']
+                self.video_ids = self.get_video_ids()
 
+    def get_video_ids(self):
+        """Получить id видеороликов из плейлиста"""
+        video_ids = []
+        playlist_items = self.youtube.playlistItems().list(part="contentDetails", playlistId=self.playlist_id,
+                                                           maxResults=50).execute()
+        while playlist_items:
+            for item in playlist_items["items"]:
+                video_ids.append(item["contentDetails"]["videoId"])
+            playlist_items = self.youtube.playlistItems().list_next(playlist_items, playlist_items)
+        return video_ids
 
-    @property
-    def total_duration(self) -> datetime.timedelta:
-        """Возвращает суммарную длительность плейлиста"""
-        total_duration = datetime.timedelta()
-        for video_id in self.video_ids:
-            video_info = self.fetch_video_info(video_id)
-            if 'items' in video_info and len(video_info['items']) > 0:
-                duration = int(video_info['items'][0]['contentDetails']['duration'])
-                total_duration += datetime.timedelta(seconds=duration)
-        return total_duration
+    def fetch_video_info(self, video_id: str) -> dict:
+        """Получает информацию о видео по его идентификатору"""
+        video_info = self.youtube.videos().list(
+            part='contentDetails',
+            id=video_id
+        ).execute()
+        return video_info
 
     def video_response(self) -> None:
-        """если информации в словаре нет возвращает информацию о плейлисте"""
+        """Возвращает информацию о видео в плейлисте"""
         if self.video_response is None:
-            self.video_response = self.youtube.videos().list(part='contentDetails,statistics',
-                                                             id=','.join(self.video_ids)).execute()
-            return self.playlist_data
+            self.video_response = self.youtube.videos().list(part='contentDetails', id=','.join(self.video_ids)).execute()
+        return self.video_response
 
     def show_best_video(self) -> str:
-        """показывает видео с наибольшим количеством лайков из плейлиста"""
+        """Показывает видео с наибольшим количеством лайков из плейлиста"""
         best_video_id = None
         max_likes = 0
         for video_id in self.video_ids:
@@ -70,14 +79,24 @@ class PlayList:
         else:
             return "No videos found in the playlist"
 
-    """показывает видео с наибольшим количеством лайков из плейлиста"""
-
     def most_viewed_video(self) -> str:
-        more_viewed: int = 0
+        """Возвращает ссылку на видео с наибольшим количеством просмотров"""
+        more_viewed = 0
         for video in self.video_response()['items']:
-            view_count: int = video['statistics']['viewCount']
+            view_count = int(video['statistics']['viewCount'])
             video_id = video['id']
-            if int(view_count) > int(more_viewed):
+            if view_count > more_viewed:
                 more_viewed = view_count
                 self.right_id = video_id
-        return "https://youtu.be/{self.right_id}"
+        return f"https://youtu.be/{self.right_id}"
+
+    @property
+    def total_duration(self) -> datetime.timedelta:
+        """Возвращает суммарную длительность плейлиста"""
+        total_duration = datetime.timedelta()
+        for video_id in self.video_ids:
+            video_info = self.fetch_video_info(video_id)
+            if 'items' in video_info and len(video_info['items']) > 0:
+                duration = isodate.parse_duration(video_info['items'][0]['contentDetails']['duration'])
+                total_duration += duration
+        return total_duration
